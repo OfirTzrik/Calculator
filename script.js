ff6e6ca98afa2aef80fe7e3e9dfab75e3c12d8a9
OfirTzrik <ofirc36@gmail.com>
2026-03-08 21:13:53 +0200

    updating display

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const screen = document.querySelector("#screen-text");
+
 function add(x, y) {
     return x + y;
 }
@@ -16,9 +18,9 @@ function divide(x, y) {
     }
 }
 
-let lhs_operand;
-let rhs_operand;
-let operator;
+let operand1 = "";
+let operand2 = "";
+let operator = "";
 
 function operate(operator, operand1, operand2) {
     switch (operator) {
@@ -33,4 +35,12 @@ function operate(operator, operand1, operand2) {
         default:
             return "Something went wrong!";
     }
+}
+
+const digit_buttons = document.querySelectorAll(".btn");
+for (const btn of digit_buttons) {
+    btn.addEventListener("click", (event) => {
+        operand1 += event.target.textContent;
+        screen.textContent = operand1;
+    });
 }
